676024ee313ea3bcc76b15d6b8dcfc0c5a8bedfc
Jeff Morgan <j.morgan00@yahoo.com>
2015-12-19 12:37:53 -0500

    code cleanup

--- js/app.js
+++ js/app.js
@@ -1,11 +1,11 @@
 
-// Enemies our player must avoid
+// Enemies our player must avoid input x and y position coordinates and speed
 var Enemy = function(x,y,speed) {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
-
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
+    
     this.sprite = 'images/enemy-bug.png';
     this.speed = speed;
     this.x = x;
@@ -15,20 +15,14 @@ var Enemy = function(x,y,speed) {
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
 Enemy.prototype.update = function(dt) {
-    // You should multiply any movement by the dt parameter
-    // which will ensure the game runs at the same speed for
-    // all computers.
+    /* You should multiply any movement by the dt parameter
+     which will ensure the game runs at the same speed for
+     all computers. */
     this.x = this.x + (Math.random() * 400 * dt);
-   
-
     //this.x = this.x + this.speed*dt;
     if (this.x > 500) {
-
         this.x = -100 + this.speed*dt;
-
     };
-
-
 };
 
 // Draw the enemy on the screen, required method for game
@@ -37,41 +31,52 @@ Enemy.prototype.render = function() {
 };
 
 
-//Player Class
+//Player Class input x and y position coordinates
 var Player = function(x,y) {
-    // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
-
-    // The image/sprite for our player, this uses
-    // a helper we've provided to easily load images
+    /* Variables applied to each of our instances go here,
+       we've provided one for you to get started
+       The image/sprite for our player, this uses
+       a helper we've provided to easily load images */
     this.x = x;
     this.y = y;
-    
-
-
-    
     this.sprite = 'images/char-boy.png';
 };
 
 // Update the player's position, required method for game
 // Parameter: dt, a time delta between ticks
 Player.prototype.update = function(dt) {
-    // You should multiply any movement by the dt parameter
-    // which will ensure the game runs at the same speed for
-    // all computers.
-        this.x; 
-        this.y ;
-        checkCollisions();
-
-
+    /* You should multiply any movement by the dt parameter
+     which will ensure the game runs at the same speed for
+     all computers. */
+    this.x; 
+    this.y ;
+    checkCollisions();
 };
 
-// Reset Player position
+ //Send player to "home" position
 Player.prototype.reset = function() {
-        this.x = 200;
-        this.y = 400;
+    this.x = 200;
+    this.y = 400;
+};
 
+ //player x-axis screen limit
+Player.prototype.xPlayerLimit = function() {
+    if (this.x <= -25) {
+            this.x = -25;
+        };
+    if (this.x >= 425) {
+            this.x = 425;
+        };
+};
 
+//player y-axis screen limit
+Player.prototype.yPlayerLimit = function() {
+    if (this.y <= 0) {
+            this.reset();
+        };
+    if (this.y >= 430) {
+           this.y = 430;
+    };
 };
 
 // Draw the player on the screen, required method for game
@@ -82,82 +87,54 @@ Player.prototype.render = function() {
 //Player Movement
 Player.prototype.handleInput = function(arrowDir) {
     if (arrowDir === 'up') { // up key held
-        this.y -= 100;
-        console.log(this.y);
-        if (this.y < 0) {
-            //this.sprite = 'images/enemy-bug.png';
-            
-            this.reset();
-        };
+        this.y -= 50;
+        this.yPlayerLimit();
     };
     if (arrowDir === 'down') { // down key held
         this.y += 100;
-
-       if (this.y >= 430) {
-           this.y = 430;
-        };
+        this.yPlayerLimit();
     };
     if (arrowDir === 'left') { // left key held
         this.x -= 100;
-
-        if (this.x <= -25) {
-            this.x = -25;
-        };
-        
+        this.xPlayerLimit();
     };
     if (arrowDir === 'right') { // right key held
         this.x += 100;
-        
-        if (this.x >= 425) {
-            this.x = 425;
-        };
-        console.log(this.x);
+        this.xPlayerLimit();
     };
 };
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
-// Place the player object in a variable called player
+// Place the player object in a variable called player 65 145 230
 
-var enemy1 = new Enemy(0,145,randomInt(0,8));
-var enemy2 = new Enemy(0,65,randomInt(250,450));
-var enemy3 = new Enemy(0,145,randomInt(250,450));
-var enemy4 = new Enemy(0,230,randomInt(250,450));
-
-/*var enemy1 = new Enemy(0,65,300);
-var enemy2 = new Enemy(0,65,200);
-var enemy3= new Enemy(0,145,Math.random()*300);
-var enemy4 = new Enemy(0,230,Math.random()*300);*/
+var enemy1 = new Enemy(40,65,randomInt(0,450));
+var enemy2 = new Enemy(475,145,randomInt(0,450));
+var enemy3 = new Enemy(350,145,randomInt(0,450));
+var enemy4 = new Enemy(220,230,randomInt(0,450));
 
 var allEnemies = [enemy1,enemy2,enemy3,enemy4];
 
 var player = new Player(200,400);
 
+//detect when player comes in contact with enemy
 function checkCollisions(){
-    
     for (var i = 0; i < allEnemies.length; i++) {
-        
-        
-        if ((allEnemies[i].x) <= player.x + 60 &&
-            
-            (allEnemies[i].x + 60) >= (player.x) &&
-            (allEnemies[i].y)<= player.y + 70 &&
+        if ((allEnemies[i].x) <= player.x + 40 &&
+            (allEnemies[i].x + 80) >= (player.x) &&
+            (allEnemies[i].y)<= player.y + 60 &&
             (allEnemies[i].y + 70) >= (player.y)) {
           console.log('ok to here');
-          player.x = 200;
-            player.y = 400;
+          //send player to "home" position
+            player.reset();
         };
     };
 };
 
-
+//generate initial random speed for each enemy
 function randomInt(min,max){
     return (Math.random()*(max-min +1)) + min;
-
 };
-
-
-
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
@@ -168,8 +145,6 @@ document.addEventListener('keyup', function(e) {
         39: 'right',
         40: 'down'
     };
-
      player.handleInput(allowedKeys[e.keyCode]);
-
 });
 
